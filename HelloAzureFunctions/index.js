b1a9a2d8b8cc568f74c719533a840834a9ba350f
Zheoden <schifferjulian@gmail.com>
2018-12-28 17:27:39 -0300

    Display text change

--- HelloAzureFunctions/index.js
+++ HelloAzureFunctions/index.js
@@ -2,9 +2,10 @@ module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
     if (req.query.name || (req.body && req.body.name)) {
+		var number = req.query.number;
         context.res = {
             // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name) + ", how are you? The number you enter is " + (prime((req.query.number || req.body.numer)) ? 'prime.' : 'not prime.')
+            body: "Hello " + (req.query.name || req.body.name) + ", how are you? The number " + number + " is " + (prime(number) ? 'prime.' : 'not prime.')
         };
     }
     else {
